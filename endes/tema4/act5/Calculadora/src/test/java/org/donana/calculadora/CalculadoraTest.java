834c862a5469a6111953dadc650c01be1790ba2b
alonsofdez23 <alonso.fernandez@iesdonana.org>
2020-03-29 21:59:51 +0200

    ENDES. Tarea8 entregada

--- endes/tema4/act5/Calculadora/src/test/java/org/donana/calculadora/CalculadoraTest.java
+++ endes/tema4/act5/Calculadora/src/test/java/org/donana/calculadora/CalculadoraTest.java
@@ -1,43 +1,51 @@
 package org.donana.calculadora;
-import org.junit.Test;
 
+// Declaración de todos los imports necesarios, que nos debe incluir VSCode
+// automáticamente
 import static org.junit.Assert.assertEquals;
 
-import org.junit.After;
-import org.junit.Before;  
-    
+import java.util.Arrays;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+// Con esta anotación le indicamos a JUnir que ejecute mis pruebas con un "runner"
+// particular, en nuestro caso, Parameterized.class que se encarga de hacer las
+// pruebas parametrizadas
+@RunWith(value = Parameterized.class)
 public class CalculadoraTest {
 
-    Calculadora calc;
-
-    @Before
-    public void before() {
-        System.out.println("before()");
-        calc = new Calculadora();
+    @Parameters
+    // Método public static que devuelve un elemento iterable de array de objetos
+    public static Iterable<Object[]> getData() {
+        // asList requiere que los elementos se pasen como un array de arrays
+        // es decir, un array bidimensional
+        return Arrays.asList(new Object[][] {
+                // Indicamos todas las pruebas {a, b, esperado}
+                { 3, 1, 4 }, { 2, 3, 5 }, { 3, 3, 6 } });
     }
 
-    @After
-    public void After() {
-        System.out.println("after()");
-        calc.clear();
+    private int a, b, esperado;
+
+    // Constructor para mi prueba con tres variables a y b (valoresd e entrada del
+    // método)
+    // y esperado (resultado esperado que devolverá el método)
+    public CalculadoraTest(int a, int b, int esperado) {
+        // Guardamos los parámetros en variables definidas anteriormente
+        this.a = a;
+        this.b = b;
+        this.esperado = esperado;
     }
-        
+
+    // Prueba sobre el método suma, que compara el valor resultado con el valor
+    // esperado
+    // calculados a partir de los parámetros a y b
     @Test
-    // Test que prueba el método Suma
     public void testSuma() {
-        System.out.println("suma()");
-        int resultado = calc.suma(3, 2);
-        int esperado = 5;
-        assertEquals(esperado, resultado);
-    }
-
-    @Test
-    // Test que prueba que el valor de ans es el esperado despúes de hacer una suma
-    public void testAnsSuma() {
-        System.out.println("ansSuma()");
-        calc.suma(3, 2);
-        int resultado = calc.ans();
-        int esperado = 5;
+        Calculadora calc = new Calculadora();
+        int resultado = calc.suma(a, b);
         assertEquals(esperado, resultado);
     }
 }
