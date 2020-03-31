9ceeba1d3f59c25f8c57e4ed90efdc94ea4489ee
alonsofdez23 <alonso.fernandez@iesdonana.org>
2020-03-31 16:37:03 +0200

    PROG. DesafioEdabit más eficiente

--- prog/2trim/covid19/DesafioEdabit.java
+++ prog/2trim/covid19/DesafioEdabit.java
@@ -1,15 +1,12 @@
 public class DesafioEdabit {
     public static int[][] squarePatch(int n) {
-        int arrayCuadrada[][] = new int[n][n];
+        int arrayCuadrado[][] = new int[n][n];
 
-        if (n == 0) {
-            return arrayCuadrada;
-        }
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                arrayCuadrada[i][j] = n;
+                arrayCuadrado[i][j] = n;
             }
         }
-        return arrayCuadrada;
+        return arrayCuadrado;
     }
 }
