22f40a5495de14a4840693924c452f30c7c8ebb3
alonsofdez23 <60521651+alonsofdez23@users.noreply.github.com>
2020-04-03 02:57:00 +0200

    PROG. StringLength no es necesario definir la class "StringLength" al hacer el println

--- prog/2trim/covid19/StringLength.java
+++ prog/2trim/covid19/StringLength.java
@@ -2,7 +2,7 @@ public class StringLength {
     public static void main(String[] args) {
         StringBuilder s1 = new StringBuilder("Alonso");
 
-        System.out.println(StringLength.longitud(s1));
+        System.out.println(longitud(s1));
     }
 
     public static int longitud(StringBuilder i) {
